cb62eb7e36ad412e3d238880ae30c0f49fa51ec6
maitee <kookmaitee@yahoo.com>
2012-03-03 21:54:45 +0100

    Moved comments of methods inside the body of the method.

--- src/de/maitee/leporello/LeporelloAssistent.py
+++ src/de/maitee/leporello/LeporelloAssistent.py
@@ -17,15 +17,15 @@ from pickle import PickleError
 from BeautifulSoup import BeautifulSoup
 # Local libraries
 
-'''
-<p>
-    This class provides meta and non-meta information ('leporello' Dict) about the 
-    leporello website which are needed to scrape its contents.
-    <b />It also provides assistant methods (static) and constants to all its 
-    subclasses for general parsing tasks.
-</p>
-'''
 class Lepistent(object):
+    '''
+    <p>
+        This class provides meta and non-meta information ('leporello' Dict) about the 
+        leporello website which are needed to scrape its contents.
+        <b />It also provides assistant methods (static) and constants to all its 
+        subclasses for general parsing tasks.
+    </p>
+    '''
     
     # Class attributes
     '''
@@ -42,7 +42,7 @@ class Lepistent(object):
     KEY_THEATRE = 'theatre'
     KEY_URL_PREFIX = 'URL_ROOT_PREFIX'
     
-    REL_PATH_DOWNLOADS_FOLDER = 'downloads/'
+    REL_PATH_DOWNLOADS_FOLDER = '../../../../downloads/'
     
     STRING_NOT_AVAILABLE = 'n/a'
     
@@ -61,14 +61,15 @@ class Lepistent(object):
         cls.CSS_CLASS_CONTENT_ITEM = info[cls.KEY_CSS_CLASS_CONTENT_ITEM]
         cls.FILE_NAME_LEPORELLO = info[cls.KEY_THEATRE]
         cls.URL_PREFIX = info[cls.KEY_URL_PREFIX]
-    '''
-    Pickles a soup in form of string for later usage.
-    @param cls: This class to reference class variables.
-    @param soup: The soup to pickle.
-    @param fileName: The file name under which the soup should be pickled.
-    '''
+    
     @classmethod
     def pickleSoup(cls, soup, file_path):
+        '''
+        Pickles a soup in form of string for later usage.
+        @param cls: This class to reference class variables.
+        @param soup: The soup to pickle.
+        @param fileName: The file name under which the soup should be pickled.
+        '''
         # Create 'downloads' directory for saving files if the directory does not exist.
         if not os.path.isdir(cls.REL_PATH_DOWNLOADS_FOLDER):
             try:
@@ -88,17 +89,17 @@ class Lepistent(object):
                 print('Pickle error: ' + str(perr))
                 print('Could not pickle soup.')
     
-    '''
-    Gets soup from a webpage/file and pickles the soup if it was fetched from a webpage.
-    @param cls: This class to reference class variables.
-    @param url: URL of the webpage from which to fetch the soup.
-    @param fileName: File name of pickled soup, if it was pickled before otherwise it is 
-    used as the file name under which the fetched soup will be pickled.
-    @return: (string) soup -A soup in form of a string, if succeeded and an empty string, 
-    if no soup could be fetched
-    '''
     @classmethod
     def getSoup(cls, url, file_path):
+        '''
+        Gets soup from a webpage/file and pickles the soup if it was fetched from a webpage.
+        @param cls: This class to reference class variables.
+        @param url: URL of the webpage from which to fetch the soup.
+        @param fileName: File name of pickled soup, if it was pickled before otherwise it is 
+        used as the file name under which the fetched soup will be pickled.
+        @return: (string) soup -A soup in form of a string, if succeeded and an empty string, 
+        if no soup could be fetched
+        '''
         soup = ''
         webpage = ''
         
@@ -128,14 +129,14 @@ class Lepistent(object):
             
         return soup
         
-    '''
-    Gets the URL of a link ('<a>' tag) from an HTML tag.
-    @param cls: This class to reference class variables.
-    @param tag: Tag that contains the <a> tag with the URL address.
-    @return: (string) url - The URL address in form a string. 
-    '''
     @classmethod
     def getURLFromTagContent(cls, tag_content):
+        '''
+        Gets the URL of a link ('<a>' tag) from an HTML tag.
+        @param cls: This class to reference class variables.
+        @param tag: Tag that contains the <a> tag with the URL address.
+        @return: (string) url - The URL address in form a string. 
+        '''
         url = cls.URL_PREFIX
     
         try:
@@ -148,44 +149,44 @@ class Lepistent(object):
             
         return url
         
-    '''
-    Creates a file path including the file name for pickling any kind of text files (webpages) 
-    by removing all non-alphanumeric characters from the name.
-    @param cls: This class to reference class variables.
-    @param path: The path to the directory in which the file will be pickled.
-    @param name: The name that will be formatted so it it can be used as the file name.
-    @param suffix: The suffix of the file name, e.g. '.html', '.txt', ... .
-    @return: (string) file_path - A formatted file path that can be used for pickling a file.
-    '''
     @classmethod
     def createFilePath(cls, path, name, suffix):
+        '''
+        Creates a file path including the file name for pickling any kind of text files (webpages) 
+        by removing all non-alphanumeric characters from the name.
+        @param cls: This class to reference class variables.
+        @param path: The path to the directory in which the file will be pickled.
+        @param name: The name that will be formatted so it it can be used as the file name.
+        @param suffix: The suffix of the file name, e.g. '.html', '.txt', ... .
+        @return: (string) file_path - A formatted file path that can be used for pickling a file.
+        '''
         # Removing all non-alphanumeric characters and appending the suffix.
         file_path = path + re.sub(r"\W+", "", name) + suffix
         
         return file_path
 
-    '''
-    Gets all tags that uses a specific CSS class.
-    @param cls: This class to reference class variables.
-    @param soup: The soup containin the HTML code that will be inspected.
-    @param tag: The HTML tag that uses the specified CSS class.
-    @param css_class: 
-    '''
     @classmethod
     def getTagsByClass(cls, soup, tag, css_class):
+        '''
+        Gets all tags that uses a specific CSS class.
+        @param cls: This class to reference class variables.
+        @param soup: The soup containin the HTML code that will be inspected.
+        @param tag: The HTML tag that uses the specified CSS class.
+        @param css_class: 
+        '''
         play_items = soup.findAll(tag, {"class": css_class})
         
         return play_items
         
-    '''
-    @warning: This method is not working. The while condition skips one tag.
-    Removes all subtrees from a soup.
-    @param soup: The soup from which to remove the subtrees.
-    @param subtree: A lambda expression that determines the subtree(s) (its a path to a tag) 
-                    that will be removed.
-    '''
     @classmethod
     def removeSubtreesFromSoup(cls, soup, f_subtree):
+        '''
+        @warning: This method is not working. The while condition skips one tag.
+        Removes all subtrees from a soup.
+        @param soup: The soup from which to remove the subtrees.
+        @param subtree: A lambda expression that determines the subtree(s) (its a path to a tag) 
+                        that will be removed.
+        '''
         # while condition does not work correctly.
         while f_subtree():
             f_subtree = f_subtree()
