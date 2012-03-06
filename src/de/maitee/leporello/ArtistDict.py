bd33086ef263f0ec6f1412ceea179324c2bc0633
Simon Lucas <simon.lucas@enough.de>
2012-03-06 14:09:23 +0100

    Implemented method _setName() which sets the first, middle and last name.

--- src/de/maitee/leporello/ArtistDict.py
+++ src/de/maitee/leporello/ArtistDict.py
@@ -15,46 +15,64 @@ class Artist(dict):
         Constructor
         '''
         dict.__init__({})
-        self.artist_item_soup = artist_item_soup
-        self.name = self._setName(name)
         self.first_name = str()
         self.middle_name = str()
         self.last_name = str()
+        self.full_name = self._setName(name)
         
         self.photo = None
         self.biography = None
         self.appearances = None
+        self.artist_item_soup = self._setDetails(artist_item_soup)
         
         
-        # 'Private' methods:
-        def _setName(self, name):
-            # TODO:
-#            name = 'name'
-            print('in _setName(' + name + ')')
-            
-            return name
+    # 'Private' methods:
+    def _setName(self, name):
+        splitted_name = name.split(' ')
+        self.first_name = splitted_name[0]
+        if len(splitted_name) > 1:
+            self.last_name = splitted_name[-1]
+            if len(splitted_name) > 2:
+                self.middle_name = ''
+                seq = splitted_name[1:-1]
+                for element in enumerate(seq):
+                    self.middle_name += element[1]
+                    if element[1] != seq[-1]:
+                        self.middle_name += ' '
+#                print('in _setName(' + self.first_name + ' ' + self.middle_name + ' ' + self.last_name + ')')
+            else:
+#                print('in _setName(' + self.first_name + self.last_name + ')')
+                pass
         
-        def _setPhoto(self):
-            # TODO:
-            print('in _setName(' + [] + ')')
-            
-            self.photo = ''
+        return name
+    
+    def _setPhoto(self):
+        # TODO:
+        photos = []
+        img_tag = self.artist_item_soup.find('img', {"class": "person-picture"})
+        print img_tag
+        print('in _setPhotos(' + str(photos) + ')')
         
-        def _setBiography(self):
-            # TODO:
-            print('in _setName(' + '' + ')')
-            
-            self.biography = ''
-            
-        def _setAppearances(self):
-            # TODO:
-            print('in _setName(' + [] + ')')
-            
-            self.appearances = list()
+        self.photo = ''
+    
+    def _setBiography(self):
+        # TODO:
+        biography = ''
+        print('in _setBiography(' + biography + ')')
         
-        def _setDetails(self, soup):
+        self.biography = ''
+        
+    def _setAppearances(self):
+        # TODO:
+        appearances = []
+        print('in _setAppearances(' + str(appearances) + ')')
+        
+        self.appearances = list()
+    
+    def _setDetails(self, soup):
+        if soup:
             self._setPhoto()
             self._setBiography()
             self._setAppearances()
-            
-            return soup
+        
+        return soup
