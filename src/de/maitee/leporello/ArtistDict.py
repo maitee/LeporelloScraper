c6d944c61194ae0a8dc13bd81e7d6852279cd60b
Simon Lucas <simon.lucas@enough.de>
2012-03-08 16:21:34 +0100

    ArtistDict.py - Replaced print by logging outputs.

--- src/de/maitee/leporello/ArtistDict.py
+++ src/de/maitee/leporello/ArtistDict.py
@@ -47,29 +47,27 @@ class Artist(dict):
     def _setRole(self, role):
         if role and role in PlayDict.PRODUCERS_CAST:
             self.producer_roles.append(role)
+            logger.info('%s - added "%s" to producer_roles.', self.full_name, role)
         else:
             self.artist_roles.append(role)
-    
+            logger.info('%s - added "%s" to artist_roles.', self.full_name, role)
     
     def _setData(self, data_list):
-        print('>>>>>>>>>> in ArtistDict._setData() <<<<<<<<<<')
-        
-        role = ''
+        role = Lepistant.NOT_AVAILABLE
         full_name = ''
-        url = ''
+        url = Lepistant.NOT_AVAILABLE
         
         for element in data_list:
-            if 'class="eventDetailPersonRole"' in str(element):
+            if 'class="eventDetailPerson"' in str(element):
+                full_name = element.string
+            elif 'class="eventDetailPersonRole"' in str(element):
                 try:
                     role = element.string.split(':')[0]
                 except:
-                    print('>>>>>>>>>> Could not extract a role from "' + str(data_list) + '" due to missing ":". ' + 
-                          'This probably means that this data_list does not have a role')
+                    logger.warning('%s - set role to "%s" since no role could be find in data_list: %s', role, data_list)
             elif 'class="eventDetailPersonLink"' in str(element):
                 full_name = element.string
                 url = Lepistant.URL_PREFIX + re.search('href=\"(.+?)\"', str(element)).group(1)
-            elif 'class="eventDetailPerson"' in str(element):
-                full_name = element.string
         
         self._setName(full_name)
         if role:
@@ -95,10 +93,10 @@ class Artist(dict):
                     self.middle_name += element[1]
                     if element[1] != seq[-1]:
                         self.middle_name += ' '
-#                print('in _setName(' + self.first_name + ' ' + self.middle_name + ' ' + self.last_name + ')')
+                logger.info('%s - set full_name, middle_name and last_name to "%s", "%s" and "%s".', 
+                            self.full_name, self.first_name, self.middle_name, self.last_name)
             else:
-#                print('in _setName(' + self.first_name + self.last_name + ')')
-                pass
+                logger.info('%s - set full_name and last_name to "%s" and "%s".', self.full_name, self.first_name, self.last_name)
     
     def _setPhoto(self, soup):
         img_tag = soup.find('img', {"class": "person-picture"})
@@ -107,6 +105,8 @@ class Artist(dict):
             self.photo = url
         else:
             self.photo = Lepistant.NOT_AVAILABLE
+            
+        logger.info('%s - set photo: "%s".', self.full_name, self.photo)
     
     def _setBiography(self, soup):
         biography_p_tag = soup.findAll('p', {"class": "person-description"})
@@ -116,6 +116,8 @@ class Artist(dict):
             self.biography = biography
         else:
             self.biography = Lepistant.NOT_AVAILABLE
+            
+        logger.info('%s - set biography: "%s...".', self.full_name, repr(self.biography[:Lepistant.LOG_MESSAGE_LENGTH]))
         
     def _setAppearances(self, soup):
         appearances = []
@@ -125,10 +127,9 @@ class Artist(dict):
             for link in appearance_link_tags:
                 appearances.append(link.string)
             self.appearances = appearances
+            logger.info('%s - set appearances: "%s".', self.full_name, self.appearances)
         except:
-            print('>>>>>>>>>> Could not set appearances due to: Appearances do not exist')
-            self.appearances = Lepistant.NOT_AVAILABLE
-        print self.appearances
+            logger.warning('Failed to set appearances for artist "%s". Therefore setting appearances to an empty list',self.full_name)
     
     def _setDetails(self, soup):
         if soup:
