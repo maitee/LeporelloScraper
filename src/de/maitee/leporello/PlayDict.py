334ddc209536645706cc8d1fe23d24878688cb20
Simon Lucas <simon.lucas@enough.de>
2012-03-08 15:39:53 +0100

    PlayDict.py - Standardized logging outputs.

--- src/de/maitee/leporello/PlayDict.py
+++ src/de/maitee/leporello/PlayDict.py
@@ -72,15 +72,15 @@ class Play(dict, Lepistant):
         self.dates = self._setDates()
         self.performances = self._setPerformances()
 
-        self.link = None
-        self.file_name_on_disk = None
+        self.link = str()
+        self.file_name_on_disk = str()
         
-        self.author = None
-        self.type = None
+        self.author = str()
+        self.type = str()
         self.summary = str()
-        self.further_info = None
-        self.critics = None
-        self.video = None
+        self.further_info = str()
+        self.critics = str()
+        self.video = str()
         self.photos = list()
         self.cast = dict()
         self.sponsors = list()
@@ -89,7 +89,7 @@ class Play(dict, Lepistant):
         
     # 'Private' methods:
     def _setDates(self):
-        rst_dates = list()
+        dates = list()
         try:
             dates_string = self.play_item_soup.findAll('p')[1].span.nextSibling
             # dates_string = "Neues Schauspielhaus / Termine: 10. | 14. | 18. September 2011, 01. | 16. | 22. Oktober 2011, 18. | 29. November 2011, 08. | 11. | 16. | 29. Dezember 2011, 27. Januar 2012, 03. | 10. | 15. | 26. Februar 2012, 27. März 2012"
@@ -110,74 +110,79 @@ class Play(dict, Lepistant):
                 for i in range(len(raw_dates) - 1):
                     # date = "10. September 2011"
                     date = raw_dates[i].lstrip() + month + ' ' + year
-                    rst_dates.append(date)
+                    dates.append(date)
                     
-                rst_dates.append(raw_dates[-1])
+                dates.append(raw_dates[-1])
+            logger.info('%s - set dates: %s', self.title, dates)
         except:
-            logger.warning('Failed to set dates. Therefore settings dates to an empty list.')
+            logger.warning('Failed to set dates for play "%s". Therefore setting dates to an empty list.', self.title)
         
-        return rst_dates 
+        return dates 
     
     def _setLocation(self):
+        location = Lepistant.NOT_AVAILABLE
+        
         try:
             # location_string = "Neues Schauspielhaus / Termine: 10. | 14. | 18. September 2011, 01. | 16. | 22. Oktober 2011, 18. | 29. November 2011, 08. | 11. | 16. | 29. Dezember 2011, 27. Januar 2012, 03. | 10. | 15. | 26. Februar 2012, 27. März 2012"    
             location_string = self.play_item_soup.findAll('p')[1].span.nextSibling
             # Ignore warning since we only need the location_part anyway.
             (location_part, seperator, dates_part) = location_string.partition('/')
             location = location_part.lstrip().rstrip()
+            logger.info('%s - set location: "%s"', self.title, location)
         except:
-            logger.warning('Failed to set location. Therefore setting location to %s.', Lepistant.NOT_AVAILABLE)
-        else:
-            location = Lepistant.NOT_AVAILABLE
+            logger.warning('Failed to set location for play "%s". Therefore setting location to %s.', self.title, Lepistant.NOT_AVAILABLE)
         
         return location
     
     def _setPerformances(self):
-        rst_performances = list()
+        performances = list()
         try:
             # Since "Theater Bremen" has always the same location for each play we can use same location for each performance.
             for date in self.dates:
                 performance = Performance(date, self.location)
-                rst_performances.append(performance)
+                performances.append(performance)
+                logger.info('%s - set performance: %s', self.title, performance.__dict__)
                 
             # Set performance type only for the first date
-            rst_performances[0].type = self._getPerformanceTypeOfFirstPerformance()
+            performances[0].type = self._getPerformanceTypeOfFirstPerformance()
         except TypeError as terr:
-            logger.warning('Failed to set performances due to: %s. Therefore setting performances to an empty list', str(terr), Lepistant.NOT_AVAILABLE)
+            logger.warning('Failed to set performances for play "%s" due to: %s. Therefore setting performances to an empty list',self.title, str(terr), Lepistant.NOT_AVAILABLE)
             
-        return rst_performances
+        return performances
     
     def _setSubtitle(self):
+        subtitle = Lepistant.NOT_AVAILABLE
+        
         try:
             subtitle_string = self.play_item_soup.p.string
             subtitle = subtitle_string.lstrip().rstrip()
+            logger.info('%s - set subtitle: "%s"', self.title, subtitle)
         except:
-            logger.warning('Failed to set subtitle. Therefore setting subtitle to %s.', Lepistant.NOT_AVAILABLE)
-        else:
-            subtitle = Lepistant.NOT_AVAILABLE
+            logger.warning('Failed to set subtitle for play "%s". Therefore setting subtitle to %s.',self.title, Lepistant.NOT_AVAILABLE)
             
         return subtitle
     
     def _setTitle(self):
+        title = Lepistant.NOT_AVAILABLE
+        
         try:
             title_string = self.play_item_soup.h5.a.string
             title = title_string.lstrip().rstrip()
+            logger.info('%s - set title: "%s"', title, title)
         except:
             logger.error('Failed to set title.')
-        else:
-            title = Lepistant.NOT_AVAILABLE
             
         return title
     
     def _getPerformanceTypeOfFirstPerformance(self):
+        performance_type = Lepistant.NOT_AVAILABLE
+        
         try:
             performance_type_string = self.play_item_soup.div.h6.string
             # performance_type_string = "<h6>Premiere</h6>"
             performance_type = performance_type_string.lstrip().rstrip()
         except:
-            logger.warning('Failed to set performance type. Therefore setting performance type to %s.', Lepistant.NOT_AVAILABLE)
-        else:
-            performance_type = Lepistant.NOT_AVAILABLE
+            logger.warning('Failed to get performance type for play "%s". Therefore returning "%s".',self.title, Lepistant.NOT_AVAILABLE)
             
         return performance_type
     
@@ -186,12 +191,12 @@ class Play(dict, Lepistant):
 #    def _getPhotoURLLinksFrom(self, div):
     
     def _getParagraphsForContent(self, content):
+        paragraphs = list()
+        
         try:
             paragraphs = self.play_detail_soup.find(text=content).findNext('div', {"class": 'toggleable-content-open'}).findAll('p')
         except:
-            logger.warning('Failed to get paragraphs. Therefore returning an empty list.')
-        else:
-            paragraphs = list()
+            logger.warning('Failed to get paragraphs for play "%s". Therefore returning an empty list.', self.title)
             
         return paragraphs;
     
@@ -201,62 +206,71 @@ class Play(dict, Lepistant):
                 subtree = soup.br
                 subtree.extract()
         except:
-            logger.warning('Failed to remove \<br\> tags from soup. Therefore returning the soup as it was.')
+            logger.warning('Failed to remove \<br\> tags from soup. Therefore returning the soup as it was. soup: %s', soup[:Lepistant.LOG_MESSAGE_LENGTH])
             
         return soup
     
     def _setSummary(self):
+        self.summary = Lepistant.NOT_AVAILABLE
+        
         try:
             summary_paragraphs = self._getParagraphsForContent('Inhalt')
             self.summary = Lepistant.formatParagraphsToString(summary_paragraphs)
+            logger.info('%s - set summary: "%s..."', self.title, self.summary[:Lepistant.LOG_MESSAGE_LENGTH])
         except:
-            logger.warning('Failed to set summary. Therefore setting summary to %s.', Lepistant.NOT_AVAILABLE)
-        else:
-            self.summary = Lepistant.NOT_AVAILABLE
+            logger.warning('Failed to set summary for play "%s". Therefore setting summary to %s.', self.title, Lepistant.NOT_AVAILABLE)
                 
     def _setCritics(self):
+        self.critics = Lepistant.NOT_AVAILABLE
+        
         try:
             critics_paragraphs = self._getParagraphsForContent('Pressestimmen')
             self.critics = Lepistant.formatParagraphsToString(critics_paragraphs)
+            logger.info('%s - set critics: "%s..."', self.title, self.critics[:Lepistant.LOG_MESSAGE_LENGTH])
         except:
-            logger.warning('Failed to set critics. Therefore setting critics to %s.', Lepistant.NOT_AVAILABLE)
-        else:
-            self.critics = Lepistant.NOT_AVAILABLE
+            logger.warning('Failed to set critics for play "%s". Therefore setting critics to %s.', self.title, Lepistant.NOT_AVAILABLE)
     
     def _setFurtherInfo(self):
+        self.further_info = Lepistant.NOT_AVAILABLE
+        
         try:
             further_info_paragraphs = self._getParagraphsForContent('Weitere Texte')
             self.further_info = Lepistant.formatParagraphsToString(further_info_paragraphs)
+            logger.info('%s - set further_info: "%s..."', self.title, self.further_info[:Lepistant.LOG_MESSAGE_LENGTH])
         except:
-            logger.warning('Failed to set further info. Therefore setting further info to %s.', Lepistant.NOT_AVAILABLE)
-        else:
-            self.further_info = Lepistant.NOT_AVAILABLE
+            logger.warning('Failed to set further_info for play "%s". Therefore setting further_info to %s.', self.title, Lepistant.NOT_AVAILABLE)
     
     def _setPhotos(self):
-        rst_photos = list()
+        photos = list()
+        
         try:
             img_tags = self.play_detail_soup.find('div', {"class": "thumbnails"}).findAll('img')
             for img_tag in img_tags:
                 img_url = Lepistant.getURLFromImageTag(img_tag)
-                rst_photos.append(img_url)
+                photos.append(img_url)
+            logger.info('%s - set photos: %s', self.title, photos)
         except AttributeError as attrerr:
-            logger.warning('Failed to set photos due to: %s. Therefore setting photos to an empty list.', str(attrerr))
+            logger.warning('Failed to set photos for play "%s" due to: %s. Therefore setting photos to an empty list.', self.title, str(attrerr))
         
-        self.photos = rst_photos
+        self.photos = photos
     
     def _setSponsors(self):
-        rst_sponsors = list()
+        sponsors = list()
+        
         try:
             img_tags = self.play_detail_soup.find('div', {"class": "sponsors clearfix"})
             for img_tag in img_tags:
                 img_url = Lepistant.getURLFromImageTag(img_tag)
-                rst_sponsors.append(img_url)
+                sponsors.append(img_url)
+            logger.info('%s - set sponsors: %s', self.title, sponsors)
         except AttributeError as attrerr:
-            logger.warning('Failed to set sponsor logos due to: %s. Therefore setting sponsors to an empty list.', str(attrerr))
+            logger.warning('Failed to set sponsor logos for play "%s" due to: %s. Therefore setting sponsors to an empty list.', self.title, str(attrerr))
 
-        self.sponsors = rst_sponsors
+        self.sponsors = sponsors
     
     def _setCast(self):
+        cast = dict()
+        
         try:
             print('>>>>>>>>>> in PlayDict._setCast() <<<<<<<<<<')
             artist_item_tags = self.play_detail_soup.findAll('h4', text=re.compile('Besetzung'))[0].parent.findNextSiblings(['span', 'a', 'br'])
@@ -269,25 +283,18 @@ class Play(dict, Lepistant):
             # Getting the last added producer role of this artist.
             role = artist.producer_roles[-1]             
             if role and role in PRODUCERS_CAST:
-                self.cast[role] = artist
+                cast[role] = artist
+                logger.info('%s - added artist  to producer\'s cast: {"%s": "%s"}', self.title, role, artist.full_name)
                 print('Added the role "' + role + '" to the producer\'s cast')
-            
         except AttributeError as attrerr:
-            logger.error('Failed to set cast due to: %s.', str(attrerr))
+            logger.error('Failed to set cast for play "%s" due to: %s.', self.title, str(attrerr))
     
     # 'Public' methods:
     def setPlayDetails(self, soup):
         self.play_detail_soup = soup
         self._setSummary()
-#        print self.summary
         self._setCritics()
-#        print self.critics
         self._setFurtherInfo()
-#        print self.further_info
-        self._setPhotos()
-#        print self.photos
         self._setSponsors()
-#        print self.sponsors
         self._setCast()
-#        print self.cast
         
