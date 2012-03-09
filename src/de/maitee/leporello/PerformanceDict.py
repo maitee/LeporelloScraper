0d10ba6eee6670b11078c5ca354c477e5a718390
Simon Lucas <simon.lucas@enough.de>
2012-03-09 17:24:08 +0100

    PerformanceDict.py - Refactored the skeleton of this class.

--- src/de/maitee/leporello/PerformanceDict.py
+++ src/de/maitee/leporello/PerformanceDict.py
@@ -4,26 +4,37 @@ Created on Feb 27, 2012
 @author: kms
 '''
 
+
+import datetime
+
+
 class Performance(dict):
     '''
     classdocs
     '''
 
 
-    def __init__(self, date=None, location=None):
+    def __init__(self, date_time, location):
         '''
         Constructor
         '''
         dict.__init__({})
-        self.date = date
+        
+        self.date = date_time
         self.location = location
         
         self.type = None
         self.cast = dict()
         
-        self.performance_detail_soup = None
+        self.soup = None
         
-    # addMemberToCast
+        
+    def setDetails(self, soup):
+        self.soup = soup
+        print('================ in PerformanceDict ==============')
+    
+    
+    # 'Private' methods:
     
     
 #    def setPerformanceDetails(self, soup):
